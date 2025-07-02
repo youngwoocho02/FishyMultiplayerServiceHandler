fec37502249ad9d74f763b816f862a36083fbad7
Array <88677523+youngwoocho02@users.noreply.github.com>
2025-07-02 17:03:44 +0900

    add custom to handler

--- FishyMultiplayerServiceHandlerExtensions.cs
+++ FishyMultiplayerServiceHandlerExtensions.cs
@@ -2,18 +2,18 @@ using Unity.Services.Multiplayer;
 
 public static class FishyMultiplayerServiceHandlerExtensions
 {
-    public static T WithFishyHandler<T>(this T options) where T : BaseSessionOptions
+    public static T WithFishyHandler<T>(this T options, FishyMultiplayerServiceHandler handler = null) where T : BaseSessionOptions
     {
-        return options.WithNetworkHandler(new FishyMultiplayerServiceHandler());
+        return options.WithNetworkHandler(handler ?? new FishyMultiplayerServiceHandler());
     }
 
-    public static T WithFishyRelayNetwork<T>(this T options, string region = null) where T : SessionOptions
+    public static T WithFishyRelayNetwork<T>(this T options, string region = null, FishyMultiplayerServiceHandler handler = null) where T : SessionOptions
     {
-        return options.WithRelayNetwork(region).WithFishyHandler();
+        return options.WithRelayNetwork(region).WithFishyHandler(handler);
     }
 
-    public static T WithFishyDirectNetwork<T>(this T options, string listenIp = "127.0.0.1", string publishIp = "127.0.0.1", int port = 7770) where T : SessionOptions
+    public static T WithFishyDirectNetwork<T>(this T options, string listenIp = "127.0.0.1", string publishIp = "127.0.0.1", int port = 7770, FishyMultiplayerServiceHandler handler = null) where T : SessionOptions
     {
-        return options.WithDirectNetwork(listenIp, publishIp, port).WithFishyHandler();
+        return options.WithDirectNetwork(listenIp, publishIp, port).WithFishyHandler(handler);
     }
 }
